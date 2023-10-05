e67fb3de9b498fa5df8ae7fac7204c00cab39c74
gregorymark254 <gregorymark254@gmail.com>
2023-10-05 13:14:01 +0300

    kilimani

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
