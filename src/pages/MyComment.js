a09eaac4317993b5449596b45088d21097e978ed
0sugo <josugo26@gmail.com>
2023-10-06 10:09:02 +0300

    Add comment section

--- src/pages/MyComment.js
+++ src/pages/MyComment.js
@@ -2,14 +2,11 @@
 import React from 'react';
 
 const PostDetails = ({ post, comments }) => {
-  // Render post details and comments here
   return (
     <div>
-      {/* Display post details */}
       <h1>{post.title}</h1>
       <p>{post.message}</p>
 
-      {/* Display comments */}
       <ul>
         {comments.map((comment) => (
           <li key={comment.id}>{comment.text}</li>
--- src/pages/Public.js
+++ src/pages/Public.js
@@ -1,4 +1,138 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addCommentToPost, getPosts, likePost } from '../redux/post/postSlice';
+
+// const Public = () => {
+//   const dispatch = useDispatch();
+//   const publicPosts = useSelector((state) => state.posts.posts);
+//   const isLoading = useSelector((state) => state.posts.isLoading);
+
+//   useEffect(() => {
+//     dispatch(getPosts());
+//   }, [dispatch]);
+
+//   const [commentText, setCommentText] = useState('');
+//   const [postIdToComment, setPostIdToComment] = useState(null);
+
+//   const handleCommentClick = (postId) => {
+//     setPostIdToComment(postId);
+//   };
+
+//   const handleCommentSubmit = async () => {
+//     try {
+//       await dispatch(addCommentToPost({ postId: postIdToComment, commentText }));
+//       setCommentText('');
+//       setPostIdToComment(null);
+//     } catch (error) {
+//       console.error('Error adding comment:', error);
+//     }
+//   };
+
+//   const handleLikeClick = async (postId) => {
+//     try {
+//       await dispatch(likePost(postId));
+//     } catch (error) {
+//       console.error('Error liking the post:', error);
+//     }
+//   };
+
+//   // Function to save content to local storage
+//   const saveToLocalStorage = (content) => {
+//     try {
+//       localStorage.setItem('savedContent', content);
+//       alert('Content saved to db!');
+//     } catch (error) {
+//       console.error('Error saving to local storage:', error);
+//       alert('Error saving to db!');
+//     }
+//   };
+
+//   return (
+//     <div className="bg-slate-100">
+//       <div className="container mx-auto p-4">
+//         <h1 className="text-3xl font-bold text-center mb-10 mt-8">Announcement</h1>
+//         <div className="flex flex-wrap justify-center gap-6 mb-10">
+//           <div className="bg-white rounded-lg w-full md:w-1/2 lg:w-1/3">
+//             <div className="p-4">
+//               <h3>
+//                 <b>Open For Discussions</b>
+//               </h3>
+//               <h4>
+//                 <u>
+//                   <i>Admin Announcement</i>
+//                 </u>
+//               </h4>
+//               <div>
+//                 {isLoading ? (
+//                   <div>
+//                     <p>Loading...</p>
+//                   </div>
+//                 ) : (
+//                   <div>
+//                     {publicPosts.map((post) => (
+//                       <div key={post.id} className="p-4 space-y-1">
+//                         <h4>
+//                           <b>{post.title}</b>
+//                         </h4>
+//                         <h5>{post.text}</h5>
+//                         <span>{post.created_at}</span>
+//                         <div className="flex flex-wrap items-center space-x-2">
+//                           <button
+//                             onClick={() => handleCommentClick(post.id)}
+//                             className="bg-blue-500 p-1 text-md text-white"
+//                           >
+//                             Comment
+//                           </button>
+//                           <button
+//                             onClick={() => handleLikeClick(post.id)}
+//                             className="bg-red-500 p-1 text-md text-white"
+//                           >
+//                             Like
+//                           </button>
+//                         </div>
+//                         {postIdToComment === post.id && (
+//                           <div>
+//                             <textarea
+//                               rows="4"
+//                               cols="50"
+//                               value={commentText}
+//                               onChange={(e) => setCommentText(e.target.value)}
+//                               placeholder="Write a comment..."
+//                             />
+//                             <button
+//                               onClick={() => saveToLocalStorage(commentText)}
+//                 className="bg-green-500 p-1 text-md text-white"
+//                             >
+//                               Submit Comment
+//                             </button>
+//                           </div>
+//                         )}
+//                         <br />
+//                         <hr />
+//                       </div>
+//                     ))}
+//                   </div>
+//                 )}
+//               </div>
+//               {/* Add a "Send" button to save content to local storage */}
+//               <button
+//                 onClick={() => saveToLocalStorage(commentText)}
+//                 className="bg-green-500 p-1 text-md text-white"
+//               >
+//                 Send
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Public;
+
+
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addCommentToPost, getPosts, likePost } from '../redux/post/postSlice';
 
@@ -11,28 +145,68 @@ const Public = () => {
     dispatch(getPosts());
   }, [dispatch]);
 
+  const [commentText, setCommentText] = useState('');
+  const [postIdToComment, setPostIdToComment] = useState(null);
+  const [commentsForPost, setCommentsForPost] = useState([]);
+
   const handleCommentClick = (postId) => {
-    // Handle comment button click, e.g., navigate to the comment page for the post
-    console.log(`Comment clicked for post ${postId}`);
+    setPostIdToComment(postId);
+    // Retrieve comments from local storage for the selected post
+    const storedComments = localStorage.getItem(`commentsForPost_${postId}`);
+    if (storedComments) {
+      setCommentsForPost(JSON.parse(storedComments));
+    } else {
+      setCommentsForPost([]);
+    }
   };
 
-  const handleAddComment = async (postId, commentText) => {
+  const handleCommentSubmit = async () => {
     try {
-      await dispatch(addCommentToPost({ postId, commentText }));
-      // Clear the comment input field or handle success as needed
+      await dispatch(addCommentToPost({ postId: postIdToComment, commentText }));
+      setCommentText('');
+
+      // Update comments in local storage for the selected post
+      const updatedComments = [...commentsForPost, commentText];
+      localStorage.setItem(`commentsForPost_${postIdToComment}`, JSON.stringify(updatedComments));
+
+      setCommentsForPost(updatedComments);
     } catch (error) {
-      // Handle any errors if necessary
       console.error('Error adding comment:', error);
     }
   };
 
   const handleLikeClick = async (postId) => {
     try {
-      // Dispatch the likePost action with the postId
       await dispatch(likePost(postId));
     } catch (error) {
-      // Handle any errors if necessary
       console.error('Error liking the post:', error);
+    }
+  };
+
+  // Function to save content to local storage
+  const saveToLocalStorage = (content) => {
+    try {
+      localStorage.setItem('savedContent', content);
+      alert('Content saved to db!');
+    } catch (error) {
+      console.error('Error saving to local storage:', error);
+      alert('Error saving to db!');
+    }
+  };
+
+  const toggleComments = (postId) => {
+    if (postIdToComment === postId) {
+      setPostIdToComment(null);
+      setCommentsForPost([]);
+    } else {
+      setPostIdToComment(postId);
+      // Retrieve comments from local storage for the selected post
+      const storedComments = localStorage.getItem(`commentsForPost_${postId}`);
+      if (storedComments) {
+        setCommentsForPost(JSON.parse(storedComments));
+      } else {
+        setCommentsForPost([]);
+      }
     }
   };
 
@@ -67,12 +241,11 @@ const Public = () => {
                         <span>{post.created_at}</span>
                         <div className="flex flex-wrap items-center space-x-2">
                           <button
-                            onClick={() => handleCommentClick(post.id)}
+                            onClick={() => toggleComments(post.id)}
                             className="bg-blue-500 p-1 text-md text-white"
                           >
-                            Comment
+                            {postIdToComment === post.id ? 'Hide Comments' : 'Show Comments'}
                           </button>
-                          
                           <button
                             onClick={() => handleLikeClick(post.id)}
                             className="bg-red-500 p-1 text-md text-white"
@@ -80,13 +253,51 @@ const Public = () => {
                             Like
                           </button>
                         </div>
+                        {postIdToComment === post.id && (
+                          <div>
+                            <textarea
+                              rows="4"
+                              cols="50"
+                              value={commentText}
+                              onChange={(e) => setCommentText(e.target.value)}
+                              placeholder="Write a comment..."
+                            />
+                            <button
+                              onClick={handleCommentSubmit}
+                              className="bg-blue-500 p-1 text-md text-white"
+                            >
+                              Submit Comment
+                            </button>
+                          </div>
+                        )}
                         <br />
                         <hr />
+                        {/* Display comments for the selected post */}
+                        {postIdToComment === post.id && commentsForPost.length > 0 && (
+                          <div>
+                            <h4>Comments:</h4>
+                            <ul>
+                              {commentsForPost.map((comment, index) => (
+                                <li key={index}>{comment}</li>
+                              ))}
+                            </ul>
+                          </div>
+                        )}
+                        {postIdToComment === post.id && commentsForPost.length === 0 && (
+                          <p>No comments for this post.</p>
+                        )}
                       </div>
                     ))}
                   </div>
                 )}
               </div>
+              {/* Add a "Send" button to save content to local storage */}
+              <button
+                onClick={() => saveToLocalStorage(commentText)}
+                className="bg-green-500 p-1 text-md text-white"
+              >
+                Send
+              </button>
             </div>
           </div>
         </div>
