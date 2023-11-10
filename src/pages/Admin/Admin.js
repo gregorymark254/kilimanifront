5b38d4c32dbc59cae1a8b30092fb679209c626b4
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-11-10 12:18:52 +0300

    Made changes in the admin page UI

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -26,14 +26,14 @@ const Admin = () => {
     <div className="min-h-screen">
       <div className="container mx-auto p-4">
         <h1 class="text-center font-bold text-blue-600 mt-20">Admin</h1>
-        <h3 class="text-center">Welcome {userInfo.email}</h3>
+        <h3 class="text-center mt-5">Welcome {userInfo.email}</h3>
 
-        <div>
-          <Link to="/users">Users</Link><br />
-          <Link to="/blogs">Community Forum</Link><br />
-          <Link to="/proposals">View Requests</Link><br />
-          <Link to="/createvote">Create Vote</Link><br />
-          <Link to="/addevents">Add Events</Link><br />
+        <div className='flex flex-col justify-center items-center mt-10'>
+          <Link className='hover:underline hover:text-blue-600' to="/users">Users</Link><br />
+          <Link className='hover:text-blue-600 hover:underline' to="/blogs">Community Forum</Link><br />
+          <Link className='hover:text-blue-600 hover:underline' to="/proposals">View Requests</Link><br />
+          <Link className='hover:text-blue-600 hover:underline' to="/createvote">Create Vote</Link><br />
+          <Link className='hover:text-blue-600 hover:underline' to="/addevents">Add Events</Link><br />
         </div>
       </div>
     </div>
