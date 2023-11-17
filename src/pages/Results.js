28980b64f0ca8342646b4ec7e6fc59ba579257c7
gregorymark254 <gregorymark254@gmail.com>
2023-11-17 23:22:39 +0300

    updates

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -4,7 +4,7 @@ const Results = () => {
   return (
     <div>
       <div className="container mx-auto p-4">
-        <h1 class="text-2xl font-bold text-center mb-8 mt-6">Results</h1>
+        <h1 className="text-2xl font-bold text-center mb-8 mt-6">Results</h1>
       </div>
     </div>
   )
